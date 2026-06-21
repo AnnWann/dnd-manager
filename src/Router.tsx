f6c46959ba8af88a3429329e902d626df42fa221
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-06-21 20:06:31 -0300

    feat: route shared party inventory

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
 import { CharacterView } from "./views/CharacterView"
-import { SyncView } from "./views/SyncView"
 import { MagicView } from "./views/MagicView"
+import { PartyInventoryView } from "./views/PartyInventoryView"
+import { SyncView } from "./views/SyncView"
 
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/character" replace />} />
-
       <Route path="/sync" element={<SyncView />} />
-
       <Route path="/character" element={<CharacterView />} />
-
+      <Route path="/party-inventory" element={<PartyInventoryView />} />
       <Route path="/magic" element={<MagicView />} />
     </Routes>
   )
