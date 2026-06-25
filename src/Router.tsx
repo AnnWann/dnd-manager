1bdb006110c9fb2bb7610938c5f78a1619d519c9
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-06-25 16:22:45 -0300

    feat: add missions route

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { CharacterView } from "./views/CharacterView"
 import { CreaturesCompendiumView } from "./views/CreaturesCompendiumView"
 import { InitiativeView } from "./views/InitiativeView"
 import { MagicView } from "./views/MagicView"
+import { MissionsView } from "./views/MissionsView"
 import { PartyInventoryView } from "./views/PartyInventoryView"
 import { SyncView } from "./views/SyncView"
 
@@ -14,6 +15,7 @@ export function AppRouter() {
       <Route path="/sync" element={<SyncView />} />
       <Route path="/character" element={<CharacterView />} />
       <Route path="/party-inventory" element={<PartyInventoryView />} />
+      <Route path="/missions" element={<MissionsView />} />
       <Route path="/creatures-compendium" element={<CreaturesCompendiumView />} />
       <Route path="/initiative" element={<InitiativeView />} />
       <Route path="/magic" element={<MagicView />} />
