70db39de4395ebfe0d69fd4dbf13dad37ab8f763
Uaná Gasparim <uanabgasparim@gmail.com>
2026-08-03 15:50:14 -0300

    config: prisma and better auth

---
+++ server/prisma.ts
@@ -0,0 +1,27 @@
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+import { PrismaClient } from "../generated/prisma/client"
+
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL não foi configurada.")
+}
+
+const adapter = new PrismaNeon({
+  connectionString,
+})
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
