8a1d35dc2e6197b3338bbc0c77a43221fe0d8097
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-05 17:11:03 -0300

    Add druid progression module

---
+++ src/data/classProgression/classes/druid/index.ts
@@ -0,0 +1,7 @@
+import { defineClassProgression } from "../../builders"
+import { druidSubclasses } from "./subclasses"
+
+export const druidProgression = defineClassProgression({
+  className: "druid",
+  subclasses: druidSubclasses,
+})
