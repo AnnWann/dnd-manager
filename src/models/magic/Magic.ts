39483645d41d047bee611fb67832067274a9c7a9
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-13 07:39:22 -0300

    Store Ki usage in magic state

--- src/models/magic/Magic.ts
+++ src/models/magic/Magic.ts
@@ -5,8 +5,13 @@ export type ChannelDivinityResource = {
   used: number
 }
 
+export type KiResource = {
+  used: number
+}
+
 export interface Magic {
   spells: CharacterSpells
   metamagic?: CharacterMetamagics
   channelDivinity?: ChannelDivinityResource
+  ki?: KiResource
 }
