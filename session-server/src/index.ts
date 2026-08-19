2633875ac63c09ce99c1f94e855c3f30bd2dfdda
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-19 08:41:18 -0300

    feat: route session runtime through equipment actor

--- session-server/src/index.ts
+++ session-server/src/index.ts
@@ -2,7 +2,7 @@ import {
   authenticateSessionConnection,
   type SessionServerEnv,
 } from "./auth";
-import { SessionActor } from "./MagicSessionActor";
+import { SessionActor } from "./EquipmentSessionActor";
 
 export { SessionActor };
 
