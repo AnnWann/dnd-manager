becbb334146a801b7f4ac2b38e49ec3d30c7f472
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-18 21:34:10 -0300

    feat(user): gate user context until protected resources are ready

--- src/layouts/userLayout.tsx
+++ src/layouts/userLayout.tsx
@@ -1,10 +1,16 @@
+import { RequireAuth } from "../auth/requireAuth"
 import { UserMagicProvider } from "../features/magic/UserMagicProvider"
+import { UserContextBoundary } from "../features/user/UserContextBoundary"
 import { AppRouter } from "../Router"
 
 export function UserLayout() {
   return (
-    <UserMagicProvider>
-      <AppRouter />
-    </UserMagicProvider>
+    <RequireAuth>
+      <UserMagicProvider>
+        <UserContextBoundary>
+          <AppRouter />
+        </UserContextBoundary>
+      </UserMagicProvider>
+    </RequireAuth>
   )
 }
