078e1b4383ce58d2b88d7df637f9a986d5ab1384
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-18 21:58:06 -0300

    feat(user): mount shared user data provider

--- src/layouts/userLayout.tsx
+++ src/layouts/userLayout.tsx
@@ -1,16 +1,19 @@
 import { RequireAuth } from "../auth/requireAuth"
 import { UserMagicProvider } from "../features/magic/UserMagicProvider"
 import { UserContextBoundary } from "../features/user/UserContextBoundary"
+import { UserDataProvider } from "../features/user/UserDataProvider"
 import { AppRouter } from "../Router"
 
 export function UserLayout() {
   return (
     <RequireAuth>
-      <UserMagicProvider>
-        <UserContextBoundary>
-          <AppRouter />
-        </UserContextBoundary>
-      </UserMagicProvider>
+      <UserDataProvider>
+        <UserMagicProvider>
+          <UserContextBoundary>
+            <AppRouter />
+          </UserContextBoundary>
+        </UserMagicProvider>
+      </UserDataProvider>
     </RequireAuth>
   )
 }
