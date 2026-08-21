6788354be32f83acdc1b589aadf2bbfa29ed870f
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-20 23:51:55 -0300

    chore: remove obsolete public layout

--- src/layouts/publicLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { PublicRouter } from "../PublicRouter"
-
-export function PublicLayout() {
-  return (
-    <div className="min-h-dvh bg-[color:var(--surface-app)] text-text">
-      <PublicRouter />
-    </div>
-  )
-}
