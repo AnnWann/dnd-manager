ea8ec18f3e613c674e9386b6633de2043c3ef107
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-21 00:05:40 -0300

    perf: preload user route boundary

--- src/features/user/userNavigationPreload.ts
+++ src/features/user/userNavigationPreload.ts
@@ -14,6 +14,7 @@ export function preloadUserNavigation(): Promise<void> {
     import("../../views/user/UserCharactersTab"),
     import("../../views/user/UserCampaignsRouteView"),
     import("../../views/user/UserSpellsTab"),
+    import("../magic/UserMagicRouteBoundary"),
   ])
     .then(() => undefined)
     .catch((error) => {
