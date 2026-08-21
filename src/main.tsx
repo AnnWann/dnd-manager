ff659dee674af360b72d5dfe5522f372f0192048
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-20 23:50:12 -0300

    perf: remove authenticated context from app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './mobileDialogs.css'
 import App from './App.tsx'
-import { I18nProvider } from './i18n/I18nContext'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '*',
-    element: (
-      <I18nProvider locale="pt-BR">
-        <App />
-      </I18nProvider>
-    ),
+    element: <App />,
   },
 ])
 
