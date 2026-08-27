126161a2af5cafc59e4fb1e398995f2911820202
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2026-08-27 17:39:22 -0300

    fix: make campaign item template handler ESM-safe

--- api-handlers/campaigns/[campaignId]/item-compendium/_template.ts
+++ api-handlers/campaigns/[campaignId]/item-compendium/_template.ts
@@ -1,14 +1,14 @@
 import {
   CampaignMemberStatus,
   CampaignRole,
-} from "../../../../generated/prisma/client"
+} from "../../../../generated/prisma/client.js"
 import {
   ApiError,
   handleApiError,
   jsonResponse,
-} from "../../../../server/api"
-import { prisma } from "../../../../server/prisma"
-import { requireSession } from "../../../../server/session"
+} from "../../../../server/api.js"
+import { prisma } from "../../../../server/prisma.js"
+import { requireSession } from "../../../../server/session.js"
 
 type RouteContext = {
   params?:
